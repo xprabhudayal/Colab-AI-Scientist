978318385fb95ef9aaaa6532cef6508eec6d66b9
Prabhudayal Vaishnav <128473068+xprabhudayal@users.noreply.github.com>
2024-08-29 20:36:22 +0530

    Update experiment.py

--- templates/nanoGPT/experiment.py
+++ templates/nanoGPT/experiment.py
@@ -349,11 +349,8 @@ def train(dataset="shakespeare_char", out_dir="run_0", seed_offset=0):
     backend = "nccl"  # 'nccl', 'gloo', etc.
     # system
     device = "cuda"  # Always use CUDA
-    dtype = (
-        "bfloat16"
-        if torch.cuda.is_available() and torch.cuda.is_bf16_supported()
-        else "float16"
-    )  # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler
+    dtype = "float16"
+    # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler
     compile = True  # do not torch compile the model on macbooks
 
     # various inits, derived attributes, I/O setup
